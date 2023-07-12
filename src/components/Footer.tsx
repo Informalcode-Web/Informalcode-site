2f1bdf4f70b54f6ec5c1a4ccfc0b662930943766
Hanifa Rifnaz <hanifarifnazz@gmail.com>
2023-07-12 21:29:45 +0530

    Merged changes and fixed footer and service cards

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,18 +7,19 @@ import { faFacebook, faInstagram, faLinkedin, faTwitter } from "@fortawesome/fre
 const Footer: React.FC = () => {
   return (
     <footer id="page-footer bg-light d-flex" className="mt-5">
-      <div className="web-footer px-5 mb-3">
+      <div className=" row web-footer px-5 mb-3">
+        <div className="col-12 col-sm-6 text-center text-sm-start">
           <a><img src={Logo} width="25" height="25"/></a>
           <span className="ms-3">
             {`\u00A9`} 2023 Informalcodes Online. All rights reserved.
           </span>
-          
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faTwitter} />
-        <FontAwesomeIcon icon={faLinkedin} />
-        <FontAwesomeIcon icon={faFacebook} />
-        <FontAwesomeIcon icon={faInstagram} />
+        </div>
+        <div className="col-12 col-sm-6 text-center text-sm-end text-secondary">
+          <FontAwesomeIcon className="px-3" icon={faTwitter} />
+          <FontAwesomeIcon className="px-3"  icon={faLinkedin} />
+          <FontAwesomeIcon className="px-3"  icon={faFacebook} />
+          <FontAwesomeIcon className="px-3" icon={faInstagram} />
+        </div>          
       </div>
     </footer>
 
