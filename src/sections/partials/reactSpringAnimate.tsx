a7744f35f22f5b3653246e33a5f0fbe1c036ab19
IamAjhere <Ajcreationsajmal@gmail.com>
2023-08-24 22:44:44 +0530

    reactSpringAnimation

---
+++ src/sections/partials/reactSpringAnimate.tsx
@@ -0,0 +1,12 @@
+export const upAnimation = {
+  from: { opacity: 0, transform: 'translateY(120px)' },
+  to: { opacity: 1, transform: 'translateY(0)' },
+  delay: 300,
+};
+
+export const logoAnimation = {
+  from: { transform: 'translateX(100%)' },
+  to: { transform: 'translateX(-100%)' },
+  config: { duration: 20000 },
+  loop: true,
+};
