7b7544e14fa8ce6a1ec307637a93a4d61a299ff5
IamAjhere <Ajcreationsajmal@gmail.com>
2023-11-09 09:26:38 +0530

    navbar-v2 updated

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -1,60 +1,54 @@
-import React, { useState } from 'react';
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-  Menu,
-  MenuItem,
-  Hidden,
-} from '@mui/material';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
-function Navbar() {
-  const [anchorEl, setAnchorEl] = useState(null);
+const navbarStyles = {
+  appBar: {
+    background: 'none',
+    boxShadow: 'none',
+    paddingLeft: '4rem',
+    paddingRight: '4rem',
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  logo: {
+    fontFamily: 'monospace',
+    fontWeight: 700,
+    color: 'black',
+  },
+  menuButton: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 600,
+    textTransform: 'none',
+  },
+  contactButton: {
+    background: 'black',
+    color: 'white',
+    borderRadius: 8,
+    fontSize: 14,
+    fontWeight: 700,
+    padding: '8px 16px',
+  },
+};
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+const Navbar = () => {
   return (
-    <div style={{ width: '100%' }}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography variant='h6'>Logo</Typography>
-          <Hidden mdUp>
-            <IconButton
-              edge='start'
-              color='inherit'
-              aria-label='menu'
-              onClick={handleClick}
-            >
-              DropDown
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>About</MenuItem>
-              <MenuItem onClick={handleClose}>Service</MenuItem>
-              <MenuItem onClick={handleClose}>Contact</MenuItem>
-            </Menu>
-          </Hidden>
-          <Hidden smDown>
-            <Button color='inherit'>About</Button>
-            <Button color='inherit'>Service</Button>
-            <Button color='inherit'>Contact</Button>
-            <Button color='primary'>Get in Touch</Button>
-          </Hidden>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position='static' style={navbarStyles.appBar}>
+      <Toolbar style={navbarStyles.toolbar}>
+        <Typography variant='h6' style={navbarStyles.logo}>
+          Logo
+        </Typography>
+
+        <Button sx={navbarStyles.menuButton}>About</Button>
+        <Button sx={navbarStyles.menuButton}>Services</Button>
+        <Button sx={navbarStyles.menuButton}>Contact</Button>
+
+        <Button variant='contained' style={navbarStyles.contactButton}>
+          Get in Touch
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
 export default Navbar;
